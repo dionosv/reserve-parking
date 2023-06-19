58b3c838e90117f747a0b01e7012be55b7305b96
dionosv <dion.hananto@binus.ac.id>
2023-06-19 16:46:17 +0700

    Upload 14

--- src/views/UserData.vue
+++ src/views/UserData.vue
@@ -2,18 +2,30 @@
   <div class="center" v-if="state">
     <table>
         <thead>
+            <th>User Id</th>
+            <th>Plate</th>
             <th>Email</th>
             <th>Username</th>
             <th>Full Name</th>
             <th>DOB</th>
+            <th>Booking Status</th>
         </thead>
         <tbody>
             <tr v-for="user in userdata" :key="user.id">
-                <td>{{ user.email }}</td>
-                <td>{{ user.username }}</td>
-                <td>{{ user.nama }}</td>
-                <td>{{ user.tgl_lahir }}</td>
+                <td>{{ user.id }}</td>
+                <td>{{ user.data.dataplate.plate.plat1 }} {{ user.data.dataplate.plate.plat2 }} {{ user.data.dataplate.plate.plat3 }}</td>
+                <td>{{ user.data.email }}</td>
+                <td>{{ user.data.username }}</td>
+                <td>{{ user.data.nama }}</td>
+                <td>{{ user.data.tgl_lahir }}</td>
+                <td>
+                    <span v-if="user.data.parking_detail.parking_status === 1">Booked</span>
+                    <span v-else-if="user.data.parking_detail.parking_status === 2">Occupied</span>
+                    <span v-else-if="user.data.parking_detail.parking_status === 4">No Booking</span>
+                </td>
+
             </tr>
+
             
         </tbody>
     </table>
@@ -31,6 +43,7 @@ export default {
     data() {
         return {
             userdata : [],
+            userid : [],
             state :false
         }
     },
@@ -43,7 +56,11 @@ export default {
             const q = query(collection(db, "userdata"))
             const querySnapshot = await getDocs(q);
             querySnapshot.forEach((doc) => {
-                this.userdata.push(doc.data())
+                const userDataWithId = {
+                    id: doc.id,
+                    data: doc.data()
+                };
+    this.userdata.push(userDataWithId);
             });
         },
 
@@ -69,5 +86,18 @@ export default {
     justify-content: center;
     align-self: center
 }
+
+th td{
+    padding: 25px;
+}
+
+.center > table > thead > th{
+    padding-left: 25px;
+}
+
+.center > table > tbody> tr> td{
+    padding-left: 25px;
+}
+
 
 </style>
